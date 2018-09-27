a79e78b72af113793479e1253c303ff91d03ac84
quanghung97 <hungcan1997@gmail.com>
2018-09-27 15:20:39 +0700

    fix view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,12 @@
     <head>
         <title>Shopping Cart</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" type="text/css" href="/css/app.css">
+        <link rel="stylesheet" type="text/css" href="css/app.css">
     </head>
     <body>
         <div id="app">
             <app-component/>
         </div>
-        <script src="/js/app.js"></script>
+        <script src="js/app.js"></script>
     </body>
 </html>
